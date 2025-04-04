0da867cdcaa525729cb77f12da3c103a9e8cd645
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 11:04:02 +0200

    Update conditions.c

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int entier = 10;
-    float reel = 3.5f;
-    double precis = 2.718;
+    int somme = 0;
 
-    // Opérations mixtes
-    float res1 = entier + reel;           // int + float
-    double res2 = reel + precis;          // float + double
-    double res3 = (double)entier / 3;     // cast pour forcer la division flottante
+    for (int i = 1; i <= 1000; i++) {
+        if (i % 11 == 0) {
+            continue; // ignorer si divisible par 11
+        }
 
-    // Affichage
-    printf("int + float = %.2f\n", res1);
-    printf("float + double = %.3f\n", res2);
-    printf("(double)int / 3 = %.3f\n", res3);
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
 
+        if (somme > 5000) {
+            break; // arrêter si on dépasse 5000
+        }
+    }
+
+    printf("Somme finale : %d\n", somme);
     return 0;
 }
