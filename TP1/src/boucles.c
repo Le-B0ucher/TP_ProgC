04b91eaa6b96b91fc3779cdcd63ca1bb6c359221
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 10:39:19 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // peut être changé par l'utilisateur
+    int compteur = 5;
 
     if (compteur >= 10) {
         printf("Le compteur doit être strictement inférieur à 10.\n");
         return 1;
     }
 
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
+    int i = 1;
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
             if (i >= 3 && j > 1 && j < i) {
                 printf("# ");
             } else {
                 printf("* ");
             }
+            j++;
         }
         printf("\n");
+        i++;
     }
 
     return 0;
