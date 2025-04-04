f1bf0e2fc1b4ad473e9be3dee1ba51503bb1a032
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 12:37:36 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // hauteur de la pyramide
+
+    for (int i = 1; i <= n; i++) {
+        // Espaces pour centrer la ligne
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Nombres croissants
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Nombres décroissants
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Pyramide terminée ✅\n");
+    return 0;
+}
 
