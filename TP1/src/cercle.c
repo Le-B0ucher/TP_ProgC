e628c78d04e992a07207ef8f0fd7f3854fc1da8a
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 09:11:55 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -2,13 +2,16 @@
 #include <math.h>
 
 int main() {
-    double rayon = 5.0;
-    double aire = M_PI * rayon * rayon;
-    double perimetre = 2 * M_PI * rayon;
+    double rayon, aire, perimetre;
 
-    printf("Rayon : %.2f\n", rayon);
-    printf("Aire : %.2f\n", aire);
-    printf("Périmètre : %.2f\n", perimetre);
+    printf("Entrez le rayon du cercle : ");
+    scanf("%lf", &rayon);
+
+    aire = M_PI * rayon * rayon;
+    perimetre = 2 * M_PI * rayon;
+
+    printf("Aire du cercle : %.2f\n", aire);
+    printf("Périmètre du cercle : %.2f\n", perimetre);
 
     return 0;
 }
