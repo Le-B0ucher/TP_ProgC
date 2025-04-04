101b8be7d297dc9df4f193bc05fd6284a9322817
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 12:11:50 +0200

    Update binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 
+void afficher_binaire(unsigned int n) {
+    int i;
+    int started = 0; // pour ne pas afficher les zéros de gauche inutiles
+
+    for (i = sizeof(n) * 8 - 1; i >= 0; i--) {
+        if (n & (1 << i)) {
+            putchar('1');
+            started = 1;
+        } else if (started) {
+            putchar('0');
+        }
+    }
+
+    if (!started) {
+        putchar('0'); // cas où n == 0
+    }
+
+    putchar('\n');
+}
+
 int main() {
-    int nombres[] = {1, 2, 3, 4, 5};
-    int taille = sizeof(nombres) / sizeof(nombres[0]);
+    unsigned int valeurs[] = {0, 4096, 65536, 65535, 1024};
+    int taille = sizeof(valeurs) / sizeof(valeurs[0]);
 
     for (int i = 0; i < taille; i++) {
-        printf("Le nombre %d en binaire est : ", nombres[i]);
-
-        for (int j = 4; j >= 0; j--) {
-            int bit = (nombres[i] >> j) & 1;
-            printf("%d", bit);
-            if (j % 4 == 0) {
-                printf(" ");
-            }
-        }
-        printf("\n");
+        printf("%u en binaire : ", valeurs[i]);
+        afficher_binaire(valeurs[i]);
     }
 
     return 0;
