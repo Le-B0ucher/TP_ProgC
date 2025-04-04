a4aba3f756234c6d370afffc27b2cd2d6273aa05
Le-B0ucher <rgp.petiteau@gmail.com>
2025-04-04 16:30:57 +0200

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    char str1[100] = "Hello";
-    char str2[100] = " World!";
-    char copie[100];
-    char concat[200];
+    char a[100], b[100], c[200];
 
-    // Longueur de str1
-    int len = 0;
-    while (str1[len] != '\0') {
-        len++;
-    }
-    printf("Longueur de str1 : %d\n", len);
+    printf("Chaîne 1 : ");
+    fgets(a, sizeof(a), stdin);
 
-    // Copie de str1 dans copie
+    printf("Chaîne 2 : ");
+    fgets(b, sizeof(b), stdin);
+
+    // Supprimer \n
     int i = 0;
-    while (str1[i] != '\0') {
-        copie[i] = str1[i];
+    while (a[i] != '\0') {
+        if (a[i] == '\n') a[i] = '\0';
         i++;
     }
-    copie[i] = '\0';
-    printf("Copie : %s\n", copie);
+    i = 0;
+    while (b[i] != '\0') {
+        if (b[i] == '\n') b[i] = '\0';
+        i++;
+    }
 
-    // Concaténation de str1 et str2 dans concat
+    // Longueur
+    int len = 0;
+    while (a[len] != '\0') len++;
+    printf("Longueur : %d\n", len);
+
+    // Copie
+    i = 0;
+    while ((c[i] = a[i]) != '\0') i++;
+    printf("Copie : %s\n", c);
+
+    // Concaténation
     int j = 0;
-    for (i = 0; str1[i] != '\0'; i++) {
-        concat[j++] = str1[i];
+    while (b[j] != '\0') {
+        c[i++] = b[j++];
     }
-    for (i = 0; str2[i] != '\0'; i++) {
-        concat[j++] = str2[i];
-    }
-    concat[j] = '\0';
-
-    printf("Concaténation : %s\n", concat);
+    c[i] = '\0';
+    printf("Concaténation : %s\n", c);
 
     return 0;
 }
